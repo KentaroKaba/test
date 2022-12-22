d01b3aceffc0ae32e1760c3dbb99dfdff86945c1
KentaroKaba <kamaboko2103@gmail.com>
2022-12-23 00:34:11 +0900

    aaaa

--- test.f90
+++ test.f90
@@ -3,4 +3,9 @@ program main
     
     print *, "HELLO GitHub"
     print *, "HELLO New branch"
+
+    ! add -> commit -> push 
+    ! git add <filename>
+    ! git commit　(-m "<comment>"でコメント追加　vimはCtrl+Iでinsert mode Ctrl+Cで戻って:wqで保存)
+    ! git push -u origin main (2回目以降はgit pushのみ)
 end program main
